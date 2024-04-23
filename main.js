c57056cfb7d90afa567d688cf7bac31149ba11c4
salvatorebriga <salvo.z2302@gmail.com>
2024-04-23 17:05:58 +0200

    super bonus

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-"use strict";
+"use strict"
 
 let actualText = document.getElementById('pass').innerHTML;
 
-let userName = prompt();
-let userSurname = prompt();
-let userColor = prompt();
-let randomN = Math.floor(Math.random() * 100);
+function myFunction(){
+    let newText = actualText;
+    let userName = document.getElementById('name').value;
+    console.log(userName);
+    let userSurname = document.getElementById('surname').value;
+    console.log(userSurname);
+    let userColor = document.getElementById('color').value;
+    console.log(userColor);
+    for(let i = 0; i < 10; i++){
+        let randomN = Math.floor(Math.random() * 100);
+        if(i <= 8){
+            newText += ` ${userName}${userSurname}${userColor}${randomN} ,`;
+            document.getElementById('pass').innerHTML = newText;
+        }
+        else{
+            newText += ` ${userName}${userSurname}${userColor}${randomN}`;
+            document.getElementById('pass').innerHTML = newText;
+        }
+        console.log(newText);
+    }
+}
 
-console.log(userName);
-console.log(userSurname);
-console.log(userColor);
-
-actualText += ` ${userName}${userSurname}${userColor}${randomN}`;
-
-console.log(actualText);
-
-document.getElementById('pass').innerHTML = actualText;
+function myReset(){
+    document.getElementById('pass').innerHTML = actualText;
+}
