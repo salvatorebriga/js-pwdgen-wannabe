398f3048348ef7f5ad18cd5eda2ee5c91382a101
salvatorebriga <salvo.z2302@gmail.com>
2024-04-23 15:37:53 +0200

    added js

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 "use strict";
 
+let actualText = document.getElementById('pass').innerHTML;
+
+let userName = prompt();
+let userSurname = prompt();
+let userColor = prompt();
+let randomN = Math.floor(Math.random() * 100);
+
+console.log(userName);
+console.log(userSurname);
+console.log(userColor);
+
+actualText += ` ${userName}${userSurname}${userColor}${randomN}`;
+
+console.log(actualText);
+
+document.getElementById('pass').innerHTML = actualText;
